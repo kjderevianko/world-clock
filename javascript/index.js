4585a2c71dc1a9e1ef93d9d1174a625993b2b3c6
kjderevianko <kathrin.derevianko@gmail.com>
2023-07-26 00:08:40 +0200

    Added moment and updated js with live data

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  // Amsterdam
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amstedamDateElement = amsterdamElement.querySelector(".date");
+  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+  let amsterdamTime = moment().tz("Europe/Amsterdam");
+  amstedamDateElement.innerHTML = amsterdamTime.format("MMMM Do YYYY");
+  amsterdamTimeElement.innerHTML = amsterdamTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  // Kyiv
+  let kyivElement = document.querySelector("#kyiv");
+  let kyivDateElement = kyivElement.querySelector(".date");
+  let kyivTimeElement = kyivElement.querySelector(".time");
+  let kyivTime = moment().tz("Europe/Kiev");
+  kyivDateElement.innerHTML = kyivTime.format("MMMM Do YYYY");
+  kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
+updateTime();
+setInterval(updateTime, 1000);
